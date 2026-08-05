c88da09a028cf2cd3fefcb46868ef8608676eb85
thomasfischersm <thomas@playposse.com>
2026-08-05 13:03:44 -0400

    Fix expiration date.

--- acroyoga/javascript/navigation.js
+++ acroyoga/javascript/navigation.js
@@ -297,7 +297,7 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     {
       "name": "Governor's Island acroyoga hangout and photoshoot on Sunday, 9/13/26: <a href='https://www.eventbrite.com/e/acroyoga-photo-shoot-and-hang-on-governors-island-tickets-1996820679405'>Sign up on Eventbrite (return students only!)</a>",
-      "date": "2026-9-5"
+      "date": "2026-9-13"
     }/*,
     {
       "name": "<span style='color: red'>Retired from teaching with occasional workshops. Follow the newsletter to find out about them!</span>",
